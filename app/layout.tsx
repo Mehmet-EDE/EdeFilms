dc3c0ba835b459e26f399d5491bddf01d3aba36f
Mehmet-EDE <mehmetedeiletisimm@gmail.com>
2023-10-29 01:18:13 +0300

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { Analytics } from '@vercel/analytics/react';
 import { Inter } from 'next/font/google'
 import '@/styles/reset.css'
 import './globals.css'
@@ -23,6 +24,7 @@ export default function RootLayout({
         <Header />
         <main>
           {children}
+          <Analytics />
         </main>
         <Footer />
       </body>
